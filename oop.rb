71ec09a5309ff045c1bf4335effe66d12618f552
Nathan Lambertson <lambertones@gmail.com>
2023-09-23 08:03:38 -0500

    add unicorn

--- oop.rb
+++ oop.rb
@@ -2,7 +2,19 @@
 # it should have a dynamic name attribute
 # it should have a color attribute, that is silver by default
 # it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
-
+class Unicorn
+    def initialize(name)
+      @name = name
+      @color = "silver"
+    end
+  
+    def say(str)
+      return "*~* #{str} *~*"
+    end
+  end
+  
+  unicorn1 = Unicorn.new("Ruby")
+  p unicorn1.say("I am pretty creature!")
 
 
 #  Write a class called Vampire
